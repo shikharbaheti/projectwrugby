0ccf319ee54e8749065aedd1b1706d3168837dc4
Shikhar Baheti <shikhar@tamu.edu>
2021-10-01 09:29:27 -0500

    Integrated a navbar with dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_09_30_042220) do
-ActiveRecord::Schema.define(version: 2021_09_28_183201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
 
   create_table "people", force: :cascade do |t|
     t.integer "person_id"
@@ -25,14 +34,6 @@ ActiveRecord::Schema.define(version: 2021_09_28_183201) do
     t.integer "person_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  create_table "admins", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "full_name"
-    t.string "uid"
-    t.string "avatar_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
 end
